cf63339f383a122fff00dfd8298b671ee9cb726a
ravyoli <ravyoli@gmail.com>
2022-05-17 12:31:05 +0300

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,7 +11,7 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string a_password_3="zxaQas12$4";
+            string a_password_3="zxaQas12$5";
             
             string password="zxaQas12$1";
             
