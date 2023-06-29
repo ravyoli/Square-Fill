9bc287d8af58c58c7a1df3cb48fa1fce92cb6c5e
ravyoli <ravyoli@gmail.com>
2023-06-29 14:32:46 +0300

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,42 +11,7 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-        /*
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        asdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffffasdffff
-        */
             string password="zxaQas12$1";
-            
-            string password2="zxaVas12$1";
-
-            string password3="ZxaVas12$1";
-            
-            
-            string password4="ZxaVas12$9";
-           
             
             int N = 52;
             table1.Text = "<table id=\"board\" cellspacing=0 cellpadding=0>";
